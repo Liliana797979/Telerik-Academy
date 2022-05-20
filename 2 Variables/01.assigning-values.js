51b12b464ec665f77c0054d27e883e5bc2f89947
Liliana797979 <76112004+Liliana797979@users.noreply.github.com>
2022-05-20 19:13:19 +0300

    Data types and variables
    Telerik Academy preparation course

---
+++ 2 Variables/01.assigning-values.js
@@ -0,0 +1,12 @@
+let person = 'Peter'; // string
+let age = 23; // number
+let isUnder30 = true; // boolean
+
+let ageIn5year = age + 5;
+
+console.log(person + ' is ' + age + '  years old.');
+console.log('He is under 30: ' + isUnder30);
+console.log('In 5 years he will be ' + ageIn5year);
+
+
+
---
+++ 2 Variables/08.reading-input.js
@@ -0,0 +1,24 @@
+// sample input
+// each line should consist of single string and a comma right after
+let input = [
+  'Peter',
+  '25',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+// read the first line of the input, no need to convert it to a string, it already is
+let input1 = gets();
+
+// read the second line and convert it to a number
+let input2 = gets();
+
+let age = Number(input2);
+let ageIn5Years = age + 5;
+
+// string concatenation
+print(input1 + ' is ' + age + ' years old.');
+
+// string interpolation. You could use this as it is way more readable
+print(`${input1} is ${age} years old.`);
